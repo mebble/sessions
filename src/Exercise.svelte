cef431650da74d9c05010668673b211c8c4d8ea3
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-12-25 13:01:57 +0530

    create set inputs elements

--- src/Exercise.svelte
+++ src/Exercise.svelte
@@ -12,15 +12,15 @@
         <tbody>
             <tr>
                 <th scope="row">1</th>
-                <td>BW+5 kg</td>
-                <td>999</td>
-                <td>Y</td>
+                <td><input type="text" name="set-1-weight" value="BW+5">kg</td>
+                <td><input type="number" name="set-1-reps" value="999"></td>
+                <td><input type="checkbox" name="set-1-done" checked></td>
             </tr>
             <tr>
                 <th scope="row">2</th>
-                <td>BW+5 kg</td>
-                <td>5</td>
-                <td>N</td>
+                <td><input type="text" name="set-2-weight" value="BW+5">kg</td>
+                <td><input type="number" name="set-2-reps" value="5"></td>
+                <td><input type="checkbox" name="set-2-done"></td>
             </tr>
         </tbody>
     </table>
