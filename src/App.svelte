4353ca9949ae584f64d635eb1b2386c5932358a3
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-12-25 12:57:23 +0530

    create exercise component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    import Exercise from "./Exercise.svelte";
 	export let name: string;
 </script>
 
@@ -8,34 +9,7 @@
     <div>
         <ul>
             <li>
-                <section>
-                    <h2>Pull up</h2>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th scope="col">Set</th>
-                                <th scope="col">Weight</th>
-                                <th scope="col">Reps</th>
-                                <th scope="col">Done</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>BW+5 kg</td>
-                                <td>999</td>
-                                <td>Y</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>BW+5 kg</td>
-                                <td>5</td>
-                                <td>N</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <button>Add set</button>
-                </section>
+                <Exercise />
             </li>
         </ul>
     </div>
---
+++ src/Exercise.svelte
@@ -0,0 +1,28 @@
+<section>
+    <h2>Pull up</h2>
+    <table>
+        <thead>
+            <tr>
+                <th scope="col">Set</th>
+                <th scope="col">Weight</th>
+                <th scope="col">Reps</th>
+                <th scope="col">Done</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>BW+5 kg</td>
+                <td>999</td>
+                <td>Y</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>BW+5 kg</td>
+                <td>5</td>
+                <td>N</td>
+            </tr>
+        </tbody>
+    </table>
+    <button>Add set</button>
+</section>
