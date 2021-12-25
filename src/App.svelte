1fcbfe96ed1ae5aaeef51e41bd74a19cb15eaffa
Neil Leeson Syiemlieh <neilsyiemlieh@gmail.com>
2021-12-25 12:39:26 +0530

    add basic ui element

--- src/App.svelte
+++ src/App.svelte
@@ -5,6 +5,41 @@
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    <div>
+        <ul>
+            <li>
+                <section>
+                    <h2>Pull up</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Set</th>
+                                <th scope="col">Weight</th>
+                                <th scope="col">Reps</th>
+                                <th scope="col">Done</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>BW+5 kg</td>
+                                <td>999</td>
+                                <td>Y</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>BW+5 kg</td>
+                                <td>5</td>
+                                <td>N</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <button>Add set</button>
+                </section>
+            </li>
+        </ul>
+    </div>
+
 </main>
 
 <style>
@@ -27,4 +62,8 @@
 			max-width: none;
 		}
 	}
+
+    /* * {
+        border: black solid 1px;
+    } */
 </style>
